eda271278be0d4002340546074683ebe3589ed32
SebastianHuizinga <107558881+SebastianHuizinga@users.noreply.github.com>
2023-07-14 10:35:52 +0200

    Rotating turret
    The first of many, unable to shoot for now they are but a peaceful observer. War is soon to come.
    
    (I added a turret sprite that can rotate but cant shoot :+1: )

---
+++ Assets/TurretScript.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class TurretScript : MonoBehaviour
+{
+
+    [Header("References")]
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
+    
+    [Header("Attributes")]
+    [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
+
+    private Transform target;
+
+
+    private void OnDrawGizmosSelected() {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null){
+            FindTarget();
+            return;
+        }
+
+        FaceTarget();
+
+       if (!CheckTargetIsInRange()){
+            target = null;
+       }
+
+    }
+
+
+
+
+    private void FindTarget(){
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0){
+            target = hits[0].transform;
+        }
+
+    }
+
+    private void FaceTarget(){
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private bool CheckTargetIsInRange(){
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+    }
+
+}
